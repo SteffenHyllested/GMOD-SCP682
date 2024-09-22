d8370397e4965926ca2f6e2765476ff9328410bb
Steffen Hyllested Pedersen <111784299+SteffenHyllested@users.noreply.github.com>
2024-09-22 21:51:41 +0200

    Moved constants to a new `config.lua` file. This should be used for configuration of the addon.

---
+++ lua/weapons/scp682/config.lua
@@ -0,0 +1,26 @@
+WALKSPEED = 240
+RUNSPEED = 360
+MAXHEALTH = 5000 -- Note: max health is multiplied by RF
+
+PRIMARY_HITBOX_RANGE = 60
+PRIMARY_HITBOX_OFFSET = 100 -- Note: this will offset the hitbox to the front of the player
+PRIMARY_COOLDOWN = 0.4
+PRIMARY_DAMAGE = 35
+
+SECONDARY_HITBOX_RANGE = 350
+SECONDARY_HITBOX_OFFSET = 0
+SECONDARY_COOLDOWN = 15
+SECONDARY_DAMAGE = 0
+
+ROAR_SELF_STUN = 2.5
+ROAR_TARGET_STUN = 5
+
+RF_KILL_REWARD = 0.05 -- Regenerative force gained on kill
+RF_DAMAGE_PENALTY_MULT = 0.001 -- How much Regenerative force is lost per damage taken
+RF_MAX = 3.00
+RF_MIN = 0.01
+RF_FREQ = 2 -- How frequently 682 regens
+RF_RATE = 0.015 -- How much (percentwise) 682 regens
+
+MODEL_SCALE_MIN = 0.7
+MODEL_SCALE_MAX = 1.2
--- lua/weapons/scp682/init.lua
+++ lua/weapons/scp682/init.lua
@@ -2,33 +2,7 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 
 include("shared.lua")
-
-local WALKSPEED = 240
-local RUNSPEED = 360
-local MAXHEALTH = 5000 -- Note: max health is multiplied by RF
-
-local PRIMARY_HITBOX_RANGE = 60
-local PRIMARY_HITBOX_OFFSET = 100 -- Note: this will offset the hitbox to the front of the player
-local PRIMARY_COOLDOWN = 0.4
-local PRIMARY_DAMAGE = 35
-
-local SECONDARY_HITBOX_RANGE = 350
-local SECONDARY_HITBOX_OFFSET = 0
-local SECONDARY_COOLDOWN = 15
-local SECONDARY_DAMAGE = 0
-
-local ROAR_SELF_STUN = 2
-local ROAR_TARGET_STUN = 5
-
-local RF_KILL_REWARD = 0.05 -- Regenerative force gained on kill
-local RF_DAMAGE_PENALTY_MULT = 0.001 -- How much Regenerative force is lost per damage taken
-local RF_MAX = 3.00
-local RF_MIN = 0.01
-local RF_FREQ = 2 -- How frequently 682 regens
-local RF_RATE = 0.015 -- How much (percentwise) 682 regens
-
-local MODEL_SCALE_MIN = 0.7
-local MODEL_SCALE_MAX = 1.2
+include("config.lua")
 
 hook.Add("StartCommand", "disable-682-crouch-jump", function(client, command)
     -- If they are 682
